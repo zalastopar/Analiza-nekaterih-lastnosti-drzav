256cf9ff07d80d403a940aef7b41d3c3876a9586
Zala Stopar <zala.stopar1@gmail.com>
2021-07-10 17:02:18 +0200

    funkcije za pridobivanje podatkov

--- uvoz-podatkov_imma.py
+++ uvoz-podatkov_imma.py
@@ -73,24 +73,67 @@ def read_file_to_string(directory, filename):
 ############################################################################################################################
 # Izluscimo podatke
 ############################################################################################################################
-'''Property--type
-Property--value '''
+
 
 # dobimo lastnosti
 def dobi_vsebino(page_content):
+    """ Funkcija poišče vse lastnosti Visitorja."""
     soup = BeautifulSoup(page_content, 'lxml')
     property = soup.find_all("div", class_= "Property--type")
     value = soup.find_all("div", class_= "Property--value")
     return [property, value]
 
 
-#TradeStation--ask-label"   ločimo na tiste z bidom in tiste s fiksno ceno:
-#cena: Current price, bid: Minimum bid -- Reserve price not met., Top bid -- Reserve price not met., bid pa ni na voljo prazno
-'''
-def najdi_ceno(page_content):
-    #poiščimo ceno
-    izraz_za_ceno
-'''
+# dobimo ceno/bid
+def oceni(page_content):
+    """ Funkcija ugotovi, ali je Visitor na prodaj po fiksni ceni 
+    ali je na dražbi ali pa ni naprodaj in poišče ceno kartice. 
+    Če Visitor ni na prodaj, mu pripiše ceno 'None'. 
+    Na koncu ustvari slovar z ustreznimi podatki."""
+
+    soup = BeautifulSoup(page_content, 'lxml')
+    slovar = {}
+
+    # izrazi, s katerimi ugotovimo način ocenjevanja vrednosti
+    izraz_za_ceno = re.compile(r'Current price', re.DOTALL)
+    izraz_za_min_bid = re.compile(r'Minimum bid', re.DOTALL)
+    izraz_za_top_bid = re.compile(r'Top bid', re.DOTALL)
+
+    # preverimo, če ima Visitor fiksno ceno
+    if bool(re.search(izraz_za_ceno, page_content)):
+        cena = soup.find("div", class_= "Overflowreact__OverflowContainer-sc-10mm0lu-0 fqMVjm Price--amount")
+        slovar['Type'] = 'Buy'
+        slovar['Price'] = cena.text
+        return slovar
+
+    # preverimo, če je na dražbi
+    elif bool(re.search(izraz_za_min_bid, page_content)):
+        bid = soup.find("div", class_= "Overflowreact__OverflowContainer-sc-10mm0lu-0 fqMVjm Price--amount")
+        slovar['Type'] = 'Min bid'
+        slovar['Price'] = bid.text
+        return slovar
+
+    # preverimo, če je na dražbi in ima ponudbo
+    elif bool(re.search(izraz_za_top_bid, page_content)):
+        bid = soup.find("div", class_= "Overflowreact__OverflowContainer-sc-10mm0lu-0 fqMVjm Price--amount")
+        slovar['Type'] = 'Top bid'
+        slovar['Price'] = bid.text
+        return slovar
+
+    # vsi ostali pa niso na prodaj
+    else: 
+        slovar['Type'] = 'Not for sale'
+        slovar['Price'] = None
+        return slovar
+
+
+
+
+
+
+
+
+
 
 ############################################################################################################################
 # Urejanje podatkov
@@ -132,15 +175,18 @@ def main(redownload=True, reparse=True):
 
     slovarji = []
 
-    for i in range(1, 50):#range(488, 10016):
+    for i in range(6249, 10000):
         time.sleep(0.1)
         # naložimo spletne strani za vsako kategorijo
         save_frontpage(link + str(i), directory, ime + str(i))
         html_data = read_file_to_string(directory, ime + str(i))
         vsebina = dobi_vsebino(html_data)
         slovar = izluscimo_text(ime + str(i), vsebina)
-
+        slo = oceni(html_data)
+        slovar.update(slo)
         slovarji.append(slovar)
+        
+
 
     print(slovarji)
 
