353710160d115dfc2dc54584b6d83ace0d1ac434
Zala Stopar <zala.stopar1@gmail.com>
2021-07-25 12:46:49 +0200

    Koncano

--- uvoz-podatkov_imma.py
+++ uvoz-podatkov_imma.py
@@ -199,7 +199,7 @@ def main(redownload=True, reparse=True):
     for i in range(1, 10000):
         #time.sleep(0.15)
         # naložimo spletne strani za vsako kategorijo
-        #save_frontpage(link + str(i), directory, ime + str(i))
+        save_frontpage(link + str(i), directory, ime + str(i))
 
         # poiščemo potrebne podatke in jih shranimo v slovar
         html_data = read_file_to_string(directory, ime + str(i))
